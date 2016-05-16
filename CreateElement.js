7c621a504c573b2ad134d91e41742fc0786de54c
Fattaneh <fatameri@hotmail.com>
2016-05-16 01:23:24 -0400

    Add files via upload

---
+++ CreateElement.js
@@ -0,0 +1,8 @@
+﻿function CreateElement(src, height, width, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.height = height;
+    img.width = width;
+    img.alt = alt;
+    document.body.appendChild(img);
+}
